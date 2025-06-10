57e23376d4f34c6eb205f1059b20dfc96ac5c711
caiofabiocosta <caiofabiocosta@gmail.com>
2025-06-10 21:01:15 +0000

    Finalizados

--- src/app/signout/page.tsx
+++ src/app/signout/page.tsx
@@ -33,7 +33,6 @@ export default function SignOutPage() {
         const { error } = await supabase.auth.signOut();
 
         if (error) {
-          console.error("Error signing out:", error);
           await showAlert("error", "Ooops!", "Não foi possível fazer logout. Tente novamente.");
           return;
         }
@@ -41,7 +40,6 @@ export default function SignOutPage() {
         await showAlert("success", "Sucesso", "Logout realizado com sucesso!");
         router.push("/login");
       } catch (error: any) {
-        console.error("Unexpected error during sign-out:", error);
         await showAlert("error", "Ooops!", "Ocorreu um erro inesperado. Tente novamente.");
       }
     };
